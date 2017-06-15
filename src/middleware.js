68d6ef3c15c993bca6d6bcd116e74b1e6f2926ab
YuTin Liu <yuting1987@gmail.com>
2017-06-15 17:50:53 +0800

    fix(middleware): fix authorization support user model

--- src/middleware.js
+++ src/middleware.js
@@ -2,9 +2,7 @@ import _ from 'lodash';
 import { UnauthorizedError } from './error';
 
 export function authentication(payload, args, context) {
-  const user = _.get(context, 'user', {});
-
-  if (!(_.isPlainObject(user) && user.id > 0)) {
+  if (!(_.get(context, 'user.id') > 0)) {
     throw new UnauthorizedError('authorization header is required');
   }
 }
