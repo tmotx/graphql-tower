c99b71cc98b05073019a24c701c195385cb8677e
YuTin Liu <yuting1987@gmail.com>
2017-11-07 09:33:01 +0800

    fix(tower): fix graphql-tower-account-kit should export from graphql-tower

--- packages/graphql-tower/src/index.js
+++ packages/graphql-tower/src/index.js
@@ -7,6 +7,7 @@ export * from 'graphql-tower-queries';
 export * from 'graphql-tower-types';
 export * from 'graphql-tower-unigue';
 
+export { default as AccountKit } from 'graphql-tower-account-kit';
 export { default as JWT } from 'graphql-tower-jwt';
 export { default as Model } from 'graphql-tower-model';
 export { default as GlobalId } from 'graphql-tower-global-id';
