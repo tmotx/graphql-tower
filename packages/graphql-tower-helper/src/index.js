8b55e3c6955ffa784b234be8ac75200c1cd2278c
YuTin Liu <yuting1987@gmail.com>
2017-11-13 15:57:59 +0800

    fix(helper): fix combine and next promise

--- packages/graphql-tower-helper/src/index.js
+++ packages/graphql-tower-helper/src/index.js
@@ -3,16 +3,16 @@ export function thunk(handler) {
 }
 
 export function combine(handler, args) {
-  const then = (...promise) => Promise.resolve(handler(args)).then(...promise);
+  const then = (...promise) => Promise.resolve(args).then(handler).then(...promise);
   return Object.assign(add => combine(handler, Object.assign({}, args, add)), args, {
-    then, promise: Object.assign(Promise.resolve(), args, { then }),
+    then, promise: Object.assign(Promise.resolve(args), args, { then }),
   });
 }
 
 export function next(handler, ...args) {
-  const then = (...promise) => Promise.resolve(handler(...args)).then(...promise);
+  const then = (...promise) => Promise.resolve(args).then(() => handler(...args)).then(...promise);
   return Object.assign((...add) => {
     const fill = args.map(value => (value === undefined ? add.shift() : value));
     return next(handler, ...fill.concat(add));
-  }, { then, promise: Object.assign(Promise.resolve(), args, { then }) });
+  }, { then, promise: Object.assign(Promise.resolve(args), args, { then }) });
 }
