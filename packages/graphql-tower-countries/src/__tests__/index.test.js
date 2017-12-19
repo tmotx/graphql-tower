8d59e0753af27d66392c37a15863092aa99d92be
YuTin <yuting1987@gmail.com>
2017-12-20 01:09:02 +0800

    fix(countries): added flags to 242

--- packages/graphql-tower-countries/src/__tests__/index.test.js
+++ packages/graphql-tower-countries/src/__tests__/index.test.js
@@ -2,7 +2,10 @@ import countries from '../';
 
 describe('countries', () => {
   it('fetch countries', () => {
-    expect(countries.length).toBe(44);
-    countries.forEach(country => expect(country.length).toBe(3));
+    expect(countries.length).toBe(242);
+    countries.forEach(country => {
+      expect(country[0].length).toBe(2);
+      expect(country.length).toBe(3);
+    });
   });
 });
