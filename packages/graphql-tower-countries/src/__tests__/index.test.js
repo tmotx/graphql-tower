6facd7796e9dc7d0ba93bf879174e34d99494b3a
YuTin <yuting1987@gmail.com>
2017-12-20 21:16:03 +0800

    fix(countries): mv flags to svg

--- packages/graphql-tower-countries/src/__tests__/index.test.js
+++ packages/graphql-tower-countries/src/__tests__/index.test.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import countries from '../';
 
 describe('countries', () => {
@@ -5,7 +6,8 @@ describe('countries', () => {
     expect(countries.length).toBe(243);
     countries.forEach(country => {
       expect(country[0].length).toBe(2);
-      expect(country.length).toBe(3);
+      expect(country.length).toBe(2);
+      expect(fs.existsSync(`${__dirname}/../flags/${country[0]}.svg`)).toBe(true);
     });
   });
 });
