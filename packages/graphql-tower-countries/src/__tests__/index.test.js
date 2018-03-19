9178f68f29982cd917beaf28a53efe58e5406c46
YuTin <yuting1987@gmail.com>
2018-03-19 22:43:22 +0800

    feat(countries): clear local name

--- packages/graphql-tower-countries/src/__tests__/index.test.js
+++ packages/graphql-tower-countries/src/__tests__/index.test.js
@@ -3,7 +3,7 @@ import countries from '../';
 
 describe('countries', () => {
   it('fetch countries', () => {
-    expect(countries.length).toBe(243);
+    expect(countries.length).toBe(242);
     countries.forEach((country) => {
       expect(country[0].length).toBe(2);
       expect(country.length).toBe(3);
