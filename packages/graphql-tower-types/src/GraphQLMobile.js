d1c61985f0afabcd25dba48ea1540a70880ba2e2
Howardcch <howard60915@gmail.com>
2019-04-02 23:28:34 +0800

    refactor(server): fix GraphQLMobile for zero-end country code

--- packages/graphql-tower-types/src/GraphQLMobile.js
+++ packages/graphql-tower-types/src/GraphQLMobile.js
@@ -2,6 +2,6 @@ import GraphQLRegexType from './GraphQLRegexType';
 
 export default new GraphQLRegexType({
   name: 'Mobile',
-  regex: /^[1-9]{1,5}0[0-9]{12}$/,
+  regex: /^[1-9]\d{0,4}0[0-9]{12}$/,
   fake: '8860000963066131',
 });
